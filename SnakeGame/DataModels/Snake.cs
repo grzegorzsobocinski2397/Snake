e9c2eee5980f7414d67d509aea96fed69de60c2b
grzegorzsobocinski2397 <43864376+grzegorzsobocinski2397@users.noreply.github.com>
2019-02-01 15:28:52 +0100

    Added moving rectangle as a player.

--- SnakeGame/DataModels/Snake.cs
+++ SnakeGame/DataModels/Snake.cs
@@ -6,7 +6,47 @@
     public class Snake
     {
         #region Public Properties
+        private int x;
 
+        public int X
+        {
+            get { return x; }
+            set { x = value; }
+        }
+        private int y;
+
+        public int Y
+        {
+            get { return y; }
+            set { y = value; }
+        }
+        private int xspeed;
+
+        public int XSpeed
+        {
+            get { return xspeed; }
+            set { xspeed = value; }
+        }
+        private int ySpeed;
+
+        public int YSpeed
+        {
+            get { return ySpeed; }
+            set { ySpeed = value; }
+        }
+
+        /// <summary>
+        /// X coordinate position of the snake on the canvas.
+        /// </summary>
+        /// <summary>
+        /// Y coordinate position of the snake on the canvas.
+        /// </summary>
+        /// <summary>
+        /// How fast does the snake move on the canvas in the X axis.
+        /// </summary>
+        /// <summary>
+        /// How fast does the snake move on the canvas in the X axis.
+        /// </summary>
         #endregion
         #region Constructor
         /// <summary>
@@ -14,9 +54,32 @@
         /// </summary>
         public Snake()
         {
+            X = 0;
+            Y = 0;
+        }
+        #endregion
+        #region Public Methods
+        public void MoveUp()
+        {
+            XSpeed = 0;
+            YSpeed = -5;
+        }
+        public void MoveDown()
+        {
+            XSpeed = 0;
+            YSpeed = 5;
+        }
+        public void MoveRight()
+        {
+            XSpeed = 5;
+            YSpeed = 0;
+        }
+        public void MoveLeft()
+        {
+            XSpeed = -5;
+            YSpeed = 0;
 
         }
         #endregion
-
     }
 }
--- SnakeGame/ViewModels/Game/GameViewModel.cs
+++ SnakeGame/ViewModels/Game/GameViewModel.cs
@@ -1,12 +1,59 @@
-﻿namespace SnakeGame
+﻿using System.Timers;
+using System.Windows.Input;
+
+namespace SnakeGame
 {
 
 
     public class GameViewModel : BaseViewModel
     {
+        #region Public Properties
+        /// <summary>
+        /// White rectangle on the canvas. This is the player.
+        /// </summary>
+        public Snake Snake { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+        public Timer Timer { get; set; }
+        #endregion
+        #region Commands
+        public ICommand UpKeyCommand { get; set; }
+        public ICommand DownKeyCommand { get; set; }
+        public ICommand RightKeyCommand { get; set; }
+        public ICommand LeftKeyCommand { get; set; }
+        #endregion
+        #region Constructor
+
+        #endregion
+        /// <summary>
+        /// Default constructor.
+        /// </summary>
         public GameViewModel()
         {
+            Snake = new Snake();
+            Timer = new Timer(1000);
+            Timer.Elapsed += new ElapsedEventHandler(Move);
+            Timer.Interval = 100;
+            Timer.Enabled = true;
 
+            // Create commands
+            UpKeyCommand = new RelayCommand(() => Snake.MoveUp());
+            DownKeyCommand = new RelayCommand(() => Snake.MoveDown());
+            RightKeyCommand = new RelayCommand(() => Snake.MoveRight());
+            LeftKeyCommand = new RelayCommand(() => Snake.MoveLeft());
+
+            StartGame();
+        }
+
+        private async void StartGame()
+        {
+
+
+        }
+        private void Move(object source, ElapsedEventArgs e)
+        {
+            X += Snake.XSpeed;
+            Y += Snake.YSpeed;
         }
     }
 }
